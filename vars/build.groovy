9e1e7007ffd6d1d8ff2febd20742f9e1ab17a6ed
sumitgit01 <sumitjoshi1988@gmail.com>
2026-03-28 20:26:26 +0530

    changes in groovy added sonarscanner path

--- vars/build.groovy
+++ vars/build.groovy
@@ -89,7 +89,7 @@ def call(Map config = [:]) {
 
             stage('Quality Gate') {
                 steps {
-                    timeout(time: 3, unit: 'MINUTES') {
+                    timeout(time: 10, unit: 'MINUTES') {
                         waitForQualityGate abortPipeline: true
                     }
                 }
