08ac52bd8d3ec9730370df17f8e6e66ee4aaf593
sumitgit01 <sumitjoshi1988@gmail.com>
2026-03-29 20:56:38 +0530

    Added stage for nexus credentials

--- vars/build.groovy
+++ vars/build.groovy
@@ -100,23 +100,23 @@ def call(Map config = [:]) {
                 }
             }
             stage('Upload images to Nexus Artifactory') {
-    steps {
-        withCredentials([usernamePassword(
-            credentialsId: 'nexus_cred',
-            usernameVariable: 'NEXUS_USER',
-            passwordVariable: 'NEXUS_PASS'
-        )]) {
-            sh '''
-            echo "$NEXUS_PASS" | docker login 192.168.68.124:8070 -u "$NEXUS_USER" --password-stdin
+                steps {
+                    withCredentials([usernamePassword(
+                        credentialsId: 'nexus_cred',
+                        usernameVariable: 'NEXUS_USER',
+                        passwordVariable: 'NEXUS_PASS'
+                    )]) {
+                        sh '''
+                        echo "$NEXUS_PASS" | docker login 192.168.68.124:8082 -u "$NEXUS_USER" --password-stdin
 
-            chmod +x build.sh
-            ./build.sh
+                        chmod +x build.sh
+                        ./build.sh
 
-            docker logout 192.168.68.124:8070
-            '''
-        }
-    }
-}
+                        docker logout 192.168.68.124:8082
+                        '''
+                    }
+                }
+            }
         }
     }
 }
