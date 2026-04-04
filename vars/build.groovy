094df8db46e597868a5df0a55615caea865d4d5e
sumitgit01 <sumitjoshi1988@gmail.com>
2026-04-04 22:32:58 +0530

    Added upload step for Helm repo

--- vars/build.groovy
+++ vars/build.groovy
@@ -7,7 +7,7 @@
         def nexusPort       = config.get('nexusPort', '8082')
         def repoName  = config.get('repoName', 'default-repo')
         def helmRepoName = config.get('helmRepoName')
-        def helmPort = congig.get('helmPort')
+        def helmPort = config.get('helmPort')
         pipeline {
             agent any
 
