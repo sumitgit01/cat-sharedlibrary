4e4bae163ba67553c10290fcd5da9d6b016c49af
sumitgit01 <sumitjoshi1988@gmail.com>
2026-04-04 14:28:08 +0530

    Helm DEploy Steps added to Jenkinsfile

--- vars/build.groovy
+++ vars/build.groovy
@@ -149,10 +149,16 @@
                             sh """
                             cd manifestbuild
                             helm version
+                            CHART_NAME=$(grep '^name:' Chart.yaml | awk '{print $2}')
+                            CHART_VERSION=$(grep '^version:' Chart.yaml | awk '{print $2}')
+
+                            echo "Chart Name: $CHART_NAME"
+                            echo "Chart Version: $CHART_VERSION"
+
                             helm package .
 
-                            curl -u $NEXUS_USER:$NEXUS_PASS \
-                            --upload-file ${HELM_REPO_NAME}-${APP_VERSION}.tgz \
+                            curl -u "$NEXUS_USER:$NEXUS_PASS" \
+                            --upload-file ${CHART_NAME}-${CHART_VERSION}.tgz \
                             http://${NEXUS_URL}/repository/${HELM_REPO_NAME}/
                             """
                         }
